55edf4203afa359018d2a5211f9e58ac48f173b8
emmanescence <64655124+emmanescence@users.noreply.github.com>
2024-09-03 20:31:06 -0300

    Update resultados.py

--- resultados.py
+++ resultados.py
@@ -1,1 +1,48 @@
+import streamlit as st
+import pandas as pd
+import requests
+import zipfile
+import io
+
+# URL del archivo comprimido
+zip_url_resultados = 'https://www.argentina.gob.ar/sites/default/files/2023_generales_1.zip'
+
+# Descargar el archivo ZIP
+response = requests.get(zip_url_resultados)
+zip_file_resultados = zipfile.ZipFile(io.BytesIO(response.content))
+
+# Listar los archivos dentro del ZIP
+archivo_csv = '2023_Generales/ResultadoElectorales_2023_Generales.csv'
+
+# Leer el archivo CSV extraído
+with zip_file_resultados.open(archivo_csv) as csv_file:
+    csv_df = pd.read_csv(csv_file)
+
+# Título de la aplicación
+st.title('Resultados Electorales 2023')
+
+# Mostrar los primeros datos para verificar la carga correcta
+st.write('**Datos Cargados:**')
+st.dataframe(csv_df.head())
+
+# Agregar un selectbox para seleccionar el cargo
+cargos = csv_df['cargo_nombre'].unique()
+cargo_seleccionado = st.selectbox('Selecciona un Cargo:', cargos)
+
+# Filtrar los datos según el cargo seleccionado
+df_filtrado = csv_df[csv_df['cargo_nombre'] == cargo_seleccionado]
+
+# Agregar un selectbox para seleccionar el circuito_id
+circuitos = df_filtrado['ciurcuito_id'].unique()
+circuito_seleccionado = st.selectbox('Selecciona un Circuito ID:', circuitos)
+
+# Filtrar los datos según el circuito seleccionado
+df_filtrado = df_filtrado[df_filtrado['ciurcuito_id'] == circuito_seleccionado]
+
+# Agrupar por agrupacion_nombre y sumar votos_cantidad
+df_resultado = df_filtrado.groupby('agrupacion_nombre')['votos_cantidad'].sum().reset_index()
+
+# Mostrar la tabla resultante
+st.write(f'**Resultados para el Cargo: {cargo_seleccionado} y Circuito ID: {circuito_seleccionado}**')
+st.dataframe(df_resultado)
 
