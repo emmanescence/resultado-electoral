9dbf636ba886baf097188cb6a383b11ee254c041
emmanescence <64655124+emmanescence@users.noreply.github.com>
2024-09-03 22:21:09 -0300

    Update resultados.py

--- resultados.py
+++ resultados.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 import zipfile
 import io
+import altair as alt
 
 # URL del archivo comprimido
 zip_url_resultados = 'https://www.argentina.gob.ar/sites/default/files/2023_generales_1.zip'
@@ -55,11 +56,31 @@ total_votos = df_resultado['votos_cantidad'].sum()
 # Calcular el porcentaje de votos para cada agrupación
 df_resultado['porcentaje'] = (df_resultado['votos_cantidad'] / total_votos) * 100
 
+# Asignar colores según la agrupación política
+colores = {
+    'JUNTOS POR EL CAMBIO': 'yellow',
+    'LA LIBERTAD AVANZA': 'purple',
+    'UNION POR LA PATRIA': 'blue',
+    'Otros': 'red'
+}
+
+# Mapear colores a las agrupaciones
+df_resultado['color'] = df_resultado['agrupacion_nombre'].map(colores).fillna('red')
+
 # Mostrar la tabla resultante
 st.write(f'**Resultados para el Cargo: {cargo_seleccionado} y Circuito ID: {circuito_seleccionado}**')
 st.dataframe(df_resultado)
 
-# Mostrar los resultados en un gráfico de barras
-st.write('**Gráfico de Resultados:**')
-st.bar_chart(df_resultado.set_index('agrupacion_nombre')['porcentaje'])
+# Crear gráfico con Altair
+chart = alt.Chart(df_resultado).mark_bar().encode(
+    x=alt.X('agrupacion_nombre:N', title='Agrupación'),
+    y=alt.Y('porcentaje:Q', title='Porcentaje de Votos'),
+    color=alt.Color('color:N', scale=None, legend=None)  # Usar los colores asignados
+).properties(
+    width=600,
+    height=400,
+    title=f'Resultados Electorales en {circuito_seleccionado}'
+)
 
+# Mostrar el gráfico
+st.altair_chart(chart)
